8e48dc5146dc582135240d609ae398eb694f5b90
hyunkyung <khc0275@naver.com>
2017-06-16 15:24:05 +0000

    added schedule app

--- schedule/models.py
+++ schedule/models.py
@@ -1,13 +1,9 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
 from django.utils import timezone
 
+
 class Post(models.Model):
-	#author = models.ForeignKey('auth.User') 외부키 충돌이 나서 일단 주석처리 했습니다, djangogirls 튜토리얼 그대로 가져오신것 같은데
-	#										 은정님도 그대로 가져오셔서 충돌이 나네요.. 일정에 author는 필요 없으니 지우셔도 될것같고 schedule 앱 안에서
-	#										 적당하게 수정해서 해주세요!
+#	author = models.ForeignKey('auth.User')
 	title = models.CharField(max_length=200)
 	text = models.TextField()
 	created_date = models.DateTimeField(
@@ -21,7 +17,3 @@ class Post(models.Model):
 
 	def __str__(self):
 		return self.title
-
-
-
-# Create your models here.
--- schedule/urls.py
+++ schedule/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^schedule$', views.index, name='schedule_index'),
+    url(r'^$', views.schedule, name='hi'),
 ]
+
