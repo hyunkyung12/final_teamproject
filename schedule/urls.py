ffe1046e94f5930cf0ca92d7ea7f31c1450bf6d7
punkyoon <cryptography@konkuk.ac.kr>
2017-06-16 16:38:57 +0000

    refactoring schedule app

--- schedule/urls.py
+++ schedule/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
-from . import views
+from schedule import views
 
 urlpatterns = [
+    # name='hi'로 설정하는 것은 바람직하지 않습니다.
     url(r'^$', views.schedule, name='hi'),
 ]
-
